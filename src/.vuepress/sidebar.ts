02d43549a8102be9fb2dd8c7c17271e0b5b32f3f
wen.xiao <wenwen.xiao@foxmail.com>
2023-12-03 02:28:41 +0800

    update

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,36 +1,5 @@
 import { sidebar } from "vuepress-theme-hope";
 
-// export default sidebar({
-//   "/": [
-//     {
-//       text: "前端",
-//       prefix: "front-end-series/",
-//       link: "front-end-series/",
-//       children: "structure",
-//     },
-//     {
-//       text: "源码阅读",
-//       prefix: "source-code/",
-//       children: "structure",
-//     },
-//     {
-//       text: "不止前端",
-//       prefix: "posts/",
-//       children: "structure",
-//     },
-//     {
-//       text: "诗歌散文",
-//       prefix: "poetry/",
-//       children: "structure",
-//     },
-//     {
-//       text: "杂谈",
-//       prefix: "posts/",
-//       children: "structure",
-//     },
-//   ],
-// });
-
 export default sidebar({
   "/front-end-series/": "structure",
   "/source-code/": "structure",
--- src/front-end-series/README.md
+++ src/front-end-series/README.md
@@ -12,6 +12,4 @@ category:
 
 - [页面展示](page.md)
 
-- [禁用展示](disable.md)
-
 - [加密展示](encrypt.md)
