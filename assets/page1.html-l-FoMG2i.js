c520cdfedd54620ceed520844cf672c3a64b8c2e
awen87 <152600139+awen87@users.noreply.github.com>
2023-12-06 06:22:38 +0000

    Deploying to gh-pages from @ awen87/awen-blog@8c3ca449b09e6ad4cb39e29f59e3e7f1421335f1 🚀

---
+++ assets/page1.html-l-FoMG2i.js
@@ -0,0 +1,14 @@
+import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as t,f as d}from"./app-30X34dwP.js";const c={},s=d(`<p>今天读了波大的两篇文章，深有感触。</p><h3 id="篇一-《因为故事-你才是与众不同的你》" tabindex="-1"><a class="header-anchor" href="#篇一-《因为故事-你才是与众不同的你》" aria-hidden="true">#</a> 篇一： 《因为故事，你才是与众不同的你》</h3><div class="language-text" data-ext="text"><pre class="language-text"><code>你为什么偏偏就喜欢我呢?
+</code></pre></div><div class="language-text" data-ext="text"><pre class="language-text"><code>你不敢面对一个不喜欢自己的她，也不愿意面对一个喜欢她的你自己
+</code></pre></div><div class="language-text" data-ext="text"><pre class="language-text"><code>不用去思考她重要不重要。她重要。你也不用去思考自己喜欢不喜欢她，你喜欢。
+关键的地方在于你的选择，一是继续掩耳盗铃告诉自己其实无所谓，二是勇敢面对，重新认真生活。
+</code></pre></div><div class="language-text" data-ext="text"><pre class="language-text"><code>你也不用刻意的去放下她。一个人之所以会在你的脑海中显得与众不同，是因为你知道她最多的故事，
+你感知到她最多的喜乐哀怒，她在你的脑海中就最立体。
+如果你放弃对生活的连接，即使随着时间不断的远去，即使慢慢离得远了，她也仍然最立体。
+所以你不用去想喜欢一个不喜欢自己的人怎么办，不用去强迫自己放下，什么都不用做，
+只需要正常的去生活，去工作，保持自己与外界的连接，自然就会有新的形象更加具体的出现。
+</code></pre></div><p>这片土地上，每天都有无数人在经历和你一样的烦恼。或逃避，或直面痛苦，我呢？尽量让自己快乐一点吧，</p><h3 id="篇二-《神奇的科斯定律》" tabindex="-1"><a class="header-anchor" href="#篇二-《神奇的科斯定律》" aria-hidden="true">#</a> 篇二： 《神奇的科斯定律》</h3><div class="language-text" data-ext="text"><pre class="language-text"><code>谁能够更好的利用资源，资源就归谁。或者说，资源通常会自动的流转到最适合它的地方
+</code></pre></div><div class="language-text" data-ext="text"><pre class="language-text"><code>如果不收拾，闲置的椅子上，会放置越来越多的衣服。(恶性循环)
+</code></pre></div><div class="language-text" data-ext="text"><pre class="language-text"><code>想要得到某样东西的最好方法，就是让自己配的上他。
+</code></pre></div><div class="language-text" data-ext="text"><pre class="language-text"><code>伤害都是互相的。而谁避免意外和伤害的成本更低，那么谁避免意外和伤害的责任就更大。
+</code></pre></div><p>站在经济学的角度去解读爱情，其实还蛮残忍的。 如果一个人因为你们之间的矛盾，能放下心中的不舍，选择分开（抛开对方是否&#39;渣&#39;不说）， 本质上是你的价值不够，好好学习吧。</p>`,13),n=[s];function l(r,x){return a(),t("div",null,n)}const i=e(c,[["render",l],["__file","page1.html.vue"]]);export{i as default};
